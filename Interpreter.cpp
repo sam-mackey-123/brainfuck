f40108c56fcd1fc66382b0fe405d80f54f1e380f
Samuel Mackey <sam_mackey@hotmail.co.uk>
2025-05-31 22:13:48 +0100

    Wrong at 3141

--- Interpreter.cpp
+++ Interpreter.cpp
@@ -11,6 +11,9 @@ void Interpreter::consume(std::istream& stream)
     while ( stream )
     {
         char a { stream.get() };
+        int x { stream.tellg() };
+
+        if (a != '\n') std::cout << m_pointer << ' ' << x << ' ' << a << '\n';
         if (a == '>'){
             m_pointer += 1;
         } else if ( a == '<') {
@@ -30,17 +33,17 @@ void Interpreter::consume(std::istream& stream)
             if (m_memory[m_pointer] == 0){
                 int depth { 1 };
                 char c;
-                while ( stream.get(c) && depth != 0){
+                while ( depth != 0 && stream.get(c)){
                     if ( c == '[' ) depth++;
                     else if ( c == ']' ) depth--;
                 } 
             }
             
         } else if ( a == ']') {
-            std::streampos p {m_jump.back() - 1};
-            m_jump.pop_back();
             if (m_memory[m_pointer] != 0){
-                stream.seekg(p);
+                stream.seekg(m_jump.back());
+            } else {
+                m_jump.pop_back();
             }
         }
 
